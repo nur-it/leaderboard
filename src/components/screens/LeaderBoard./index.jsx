4856f010ea30d4a59802a15ce3983772d7656613
devwithfarshi <devwithfarshi@gmail.com>
2024-05-31 19:50:54 +0600

    ✅ update: winner timer added

--- src/components/screens/LeaderBoard./index.jsx
+++ src/components/screens/LeaderBoard./index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import LeaderBoardTopPart from "./LeaderBoardTopPart";
 import Card from "./Card";
+import LeaderBoardTimer from "./Timer";
 
 const Leaderboard = () => {
   const [selectedTab, setSelectedTab] = useState(1);
@@ -15,24 +16,7 @@ const Leaderboard = () => {
           <Card />
           <div>
             <Card />
-            <ul className="mt-6 flex justify-center items-center gap-2.5 *:rounded-lg *:bg-primary-timer-bg  *:w-[50px] *:h-[53px] *:flex *:items-center *:justify-center *:flex-col *:font-bold [&_h4]:text-light-text [&_h4]:text-base [&_p]:text-primary-timer-text [&_p]:text-11">
-              <li>
-                <h4>25</h4>
-                <p>days</p>
-              </li>
-              <li>
-                <h4>16</h4>
-                <p>hrs</p>
-              </li>
-              <li>
-                <h4>34</h4>
-                <p>mins</p>
-              </li>
-              <li>
-                <h4>56</h4>
-                <p>sec</p>
-              </li>
-            </ul>
+            <LeaderBoardTimer />
           </div>
           <Card />
         </div>
