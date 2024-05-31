76962c4d9df866bc702d00d89c55b83c681da2c6
devwithfarshi <devwithfarshi@gmail.com>
2024-05-31 11:49:01 +0600

    ✅ add: navbar added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/shared/Header";
+
+const App = () => {
+  return (
+    <>
+      <main className="h-screen bg-[url(./bgs/background.png)] bg-no-repeat bg-center bg-cover text-white">
+        <Header />
+      </main>
+    </>
+  );
+};
+export default App;
